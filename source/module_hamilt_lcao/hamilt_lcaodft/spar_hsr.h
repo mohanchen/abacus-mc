3a8c14b312350ae10f23d61d0812711d7b836d10
mohanchen <mohan.chen.chen.mohan@gmail.com>
2024-04-05 17:52:19 +0800

    update sparse matrix

--- source/module_hamilt_lcao/hamilt_lcaodft/spar_hsr.h
+++ source/module_hamilt_lcao/hamilt_lcaodft/spar_hsr.h
@@ -1,79 +1,27 @@
+#ifndef SPARSE_FORMAT_HSR_H 
+#define SPARSE_FORMAT_HSR_H
 
+namespace sparse_format
+{
 
-void LCAO_Hamilt::cal_HSR(
+void cal_HSR(
+		const int &current_spin, 
+		const double &sparse_thr, 
+		const int (&nmp)[3], 
+		hamilt::Hamilt<std::complex<double>>* p_ham);
+
+void cal_HContainer_d(
 		const int &current_spin, 
 		const double &sparse_threshold, 
-		const int (&nmp)[3], 
-		hamilt::Hamilt<std::complex<double>>* p_ham)
-{
-    ModuleBase::TITLE("sparse_matrix","cal_HSR");
+		const hamilt::HContainer<double>& hR, 
+		std::map<Abfs::Vector3_Order<int>, std::map<size_t, std::map<size_t, double>>>& target);
 
-    sparse_format::set_R_range(*this->LM);
+void cal_HContainer_cd(
+		const int &current_spin, 
+		const double &sparse_threshold, 
+		const hamilt::HContainer<std::complex<double>>& hR, 
+		std::map<Abfs::Vector3_Order<int>, 
+		std::map<size_t, std::map<size_t, std::complex<double>>>>& target);
 
-    //cal_STN_R_sparse(current_spin, sparse_threshold);
-    if(GlobalV::NSPIN!=4)
-    {
-        hamilt::HamiltLCAO<std::complex<double>, double>* p_ham_lcao = 
-        dynamic_cast<hamilt::HamiltLCAO<std::complex<double>, double>*>(p_ham);
 
-		this->cal_HContainer_sparse_d(current_spin, 
-				sparse_threshold, 
-				*(p_ham_lcao->getHR()), 
-				this->LM->HR_sparse[current_spin]);
-
-		this->cal_HContainer_sparse_d(current_spin, 
-				sparse_threshold, 
-				*(p_ham_lcao->getSR()), 
-				this->LM->SR_sparse);
-    }
-    else
-    {
-        hamilt::HamiltLCAO<std::complex<double>, std::complex<double>>* p_ham_lcao = 
-        dynamic_cast<hamilt::HamiltLCAO<std::complex<double>, std::complex<double>>*>(p_ham);
-
-        this->cal_HContainer_sparse_cd(current_spin, 
-        sparse_threshold, 
-        *(p_ham_lcao->getHR()), 
-        this->LM->HR_soc_sparse);
-
-        this->cal_HContainer_sparse_cd(current_spin, 
-        sparse_threshold, 
-        *(p_ham_lcao->getSR()), 
-        this->LM->SR_soc_sparse);
-    }
-
-    // only old DFT+U method need to cal extra contribution to HR
-    if (GlobalV::dft_plus_u == 2)
-    {
-        if (GlobalV::NSPIN != 4)
-        {
-            cal_HR_dftu(current_spin, sparse_threshold);
-        }
-        else
-        {
-            cal_HR_dftu_soc(current_spin, sparse_threshold);
-        }
-    }
-
-#ifdef __EXX
-#ifdef __MPI
-    // if EXX is considered
-    if( GlobalC::exx_info.info_global.cal_exx )
-    {
-		if(GlobalC::exx_info.info_ri.real_number)
-		{
-			this->cal_HR_exx_sparse(current_spin, sparse_threshold, nmp, *this->LM->Hexxd);
-		}
-		else
-		{
-			this->cal_HR_exx_sparse(current_spin, sparse_threshold, nmp, *this->LM->Hexxc);
-		}
-	}
-#endif // __MPI
-#endif // __EXX
-
-    clear_zero_elements(current_spin, sparse_threshold);
-
-    return;
 }
-
