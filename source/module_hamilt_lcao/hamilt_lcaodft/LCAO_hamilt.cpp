812df48990badecb0e4f66d7e4d55ef5d336cad8
mohanchen <mohan.chen.chen.mohan@gmail.com>
2024-04-05 17:58:16 +0800

    update LCAO_hamilt.cpp

--- source/module_hamilt_lcao/hamilt_lcaodft/LCAO_hamilt.cpp
+++ source/module_hamilt_lcao/hamilt_lcaodft/LCAO_hamilt.cpp
@@ -32,83 +32,10 @@ LCAO_Hamilt::~LCAO_Hamilt()
     }
 }
 
-void LCAO_Hamilt::cal_HSR_sparse(
-		const int &current_spin, 
-		const double &sparse_threshold, 
-		const int (&nmp)[3], 
-		hamilt::Hamilt<std::complex<double>>* p_ham)
+
+void LCAO_Hamilt::cal_STN_R_for_T(const double &sparse_threshold)
 {
-    ModuleBase::TITLE("LCAO_Hamilt","cal_HSR_sparse");
-
-    sparse_format::set_R_range(*this->LM);
-
-    //cal_STN_R_sparse(current_spin, sparse_threshold);
-    if(GlobalV::NSPIN!=4)
-    {
-        hamilt::HamiltLCAO<std::complex<double>, double>* p_ham_lcao = 
-        dynamic_cast<hamilt::HamiltLCAO<std::complex<double>, double>*>(p_ham);
-
-		this->cal_HContainer_sparse_d(current_spin, 
-				sparse_threshold, 
-				*(p_ham_lcao->getHR()), 
-				this->LM->HR_sparse[current_spin]);
-
-		this->cal_HContainer_sparse_d(current_spin, 
-				sparse_threshold, 
-				*(p_ham_lcao->getSR()), 
-				this->LM->SR_sparse);
-    }
-    else
-    {
-        hamilt::HamiltLCAO<std::complex<double>, std::complex<double>>* p_ham_lcao = 
-        dynamic_cast<hamilt::HamiltLCAO<std::complex<double>, std::complex<double>>*>(p_ham);
-
-        this->cal_HContainer_sparse_cd(current_spin, 
-        sparse_threshold, 
-        *(p_ham_lcao->getHR()), 
-        this->LM->HR_soc_sparse);
-
-        this->cal_HContainer_sparse_cd(current_spin, 
-        sparse_threshold, 
-        *(p_ham_lcao->getSR()), 
-        this->LM->SR_soc_sparse);
-    }
-
-    // only old DFT+U method need to cal extra contribution to HR
-    if (GlobalV::dft_plus_u == 2)
-    {
-        if (GlobalV::NSPIN != 4)
-        {
-            cal_HR_dftu(current_spin, sparse_threshold);
-        }
-        else
-        {
-            cal_HR_dftu_soc(current_spin, sparse_threshold);
-        }
-    }
-
-#ifdef __EXX
-#ifdef __MPI
-    if( GlobalC::exx_info.info_global.cal_exx )
-    {
-		if(GlobalC::exx_info.info_ri.real_number)
-		{
-			this->cal_HR_exx_sparse(current_spin, sparse_threshold, nmp, *this->LM->Hexxd);
-		}
-		else
-		{
-			this->cal_HR_exx_sparse(current_spin, sparse_threshold, nmp, *this->LM->Hexxc);
-		}
-	}
-#endif // __MPI
-#endif // __EXX
-
-    clear_zero_elements(current_spin, sparse_threshold);
-}
-
-void LCAO_Hamilt::cal_STN_R_sparse_for_T(const double &sparse_threshold)
-{
-    ModuleBase::TITLE("LCAO_Hamilt","cal_STN_R_sparse_for_T");
+    ModuleBase::TITLE("LCAO_Hamilt","cal_STN_R_for_T");
 
     int index = 0;
     ModuleBase::Vector3<double> dtau, tau1, tau2;
@@ -216,9 +143,10 @@ void LCAO_Hamilt::cal_STN_R_sparse_for_T(const double &sparse_threshold)
     return;
 }
 
-void LCAO_Hamilt::cal_SR_sparse(const double &sparse_threshold, hamilt::Hamilt<std::complex<double>>* p_ham)
+
+void LCAO_Hamilt::cal_SR(const double &sparse_threshold, hamilt::Hamilt<std::complex<double>>* p_ham)
 {
-    ModuleBase::TITLE("LCAO_Hamilt","cal_SR_sparse");
+    ModuleBase::TITLE("LCAO_Hamilt","cal_SR");
     sparse_format::set_R_range(*this->LM);
     //cal_STN_R_sparse(current_spin, sparse_threshold);
     if(GlobalV::NSPIN!=4)
@@ -235,11 +163,12 @@ void LCAO_Hamilt::cal_SR_sparse(const double &sparse_threshold, hamilt::Hamilt<s
     }
 }
 
-void LCAO_Hamilt::cal_TR_sparse(
+
+void LCAO_Hamilt::cal_TR(
 		LCAO_gen_fixedH &gen_h,
 		const double &sparse_threshold)
 {
-    ModuleBase::TITLE("LCAO_Hamilt","cal_TR_sparse");
+    ModuleBase::TITLE("LCAO_Hamilt","cal_TR");
     
     //need to rebuild T(R)
     this->LM->Hloc_fixedR.resize(this->LM->ParaV->nnr);
@@ -255,7 +184,9 @@ void LCAO_Hamilt::cal_TR_sparse(
 
 
 // in case there are elements smaller than the threshold
-void LCAO_Hamilt::clear_zero_elements(const int &current_spin, const double &sparse_threshold)
+void LCAO_Hamilt::clear_zero_elements(
+		const int &current_spin, 
+		const double &sparse_threshold)
 {
     if(GlobalV::NSPIN != 4)
     {
