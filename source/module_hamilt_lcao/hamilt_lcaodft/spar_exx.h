5b1f577a71854d067985d36ea8719a5b52419d96
mohanchen <mohan.chen.chen.mohan@gmail.com>
2024-04-06 15:15:08 +0800

    in some sense, the exx code is a disaster

--- source/module_hamilt_lcao/hamilt_lcaodft/spar_exx.h
+++ source/module_hamilt_lcao/hamilt_lcaodft/spar_exx.h
@@ -1,6 +1,8 @@
 #ifndef SPARSE_FORMAT_EXX_H 
 #define SPARSE_FORMAT_EXX_H
 
+#ifdef __EXX
+
 #include <vector>
 #include <map>
 #include <array>
@@ -9,20 +11,19 @@
 #include <RI/ri/Cell_Nearest.h>
 
 // use LCAO_Matrix
-#include "module_hamilt_lcao/hamilt_lcaodft/LCAO_matrix.h"
+#include "module_hamilt_lcao/hamilt_lcaodft/spar_exx.h"
 
 namespace sparse_format
 {
 
-#ifdef __EXX
     template<typename Tdata> void cal_HR_exx(
             LCAO_Matrix &lm,
             const int &current_spin,
             const double &sparse_thr,
             const int (&nmp)[3],
             const std::vector< std::map <int, std::map < std::pair<int, std::array<int,3>>, RI::Tensor<Tdata> > >>& Hexxs);
-#endif
 
 }
 
+#endif
 #endif
