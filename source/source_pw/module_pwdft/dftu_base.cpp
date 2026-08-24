6971024227964fd235144b42a915e4c1f6bf2c76
abacus_fixer <mohanchen@pku.eud.cn>
2026-08-24 12:08:55 +0800

    refactor(dftu): phase 1 - scaffold dftu_base.h/.cpp skeleton
    - Add empty Plus_U_Base class in source_pw/module_pwdft/
    - Wire into CMakeLists.txt and Makefile.Objects
    - No functional change; dftu.h untouched

---
+++ source/source_pw/module_pwdft/dftu_base.cpp
@@ -0,0 +1,9 @@
+#include "source_pw/module_pwdft/dftu_base.h"
+
+
+Plus_U_Base::Plus_U_Base()
+{}
+
+
+Plus_U_Base::~Plus_U_Base()
+{}
---
+++ source/source_pw/module_pwdft/dftu_base.h
@@ -0,0 +1,13 @@
+#ifndef DFTU_BASE_H
+#define DFTU_BASE_H
+
+
+class Plus_U_Base
+{
+  public:
+    Plus_U_Base();
+    ~Plus_U_Base();
+};
+
+
+#endif
