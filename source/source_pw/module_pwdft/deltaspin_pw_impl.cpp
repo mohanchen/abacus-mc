cd6e047e22436a49aaf7c3e06a741419486f6d58
abacus_fixer <mohanchen@pku.eud.cn>
2026-08-25 11:49:56 +0800

    deltaspin: move calculate_delta_hcc to module_pwdft
    Relocate the PW-specific
    SpinConstrain<std::complex<double>>::calculate_delta_hcc() member function
    from module_deltaspin/cal_mw_from_lambda.cpp to
    module_pwdft/deltaspin_pw_impl.cpp.
    
    calculate_delta_hcc() builds the DeltaSpin correction to the subspace
    Hamiltonian (H += becp^d * delta_lambda * becp) using PW projector
    coefficients. It is only called by update_psi_charge_pw_cpu/gpu, which
    are PW-specific and will be moved in the next step.
    
    Changes:
    - module_pwdft/deltaspin_pw_impl.cpp: add calculate_delta_hcc()
      implementation; add #include "source_base/kernels/math_kernel_op.h"
      for ModuleBase::gemm_op used by the function.
    - module_deltaspin/cal_mw_from_lambda.cpp: remove calculate_delta_hcc()
      function body and its doxygen comment block; leave a brief comment
      noting the function has been relocated. All call sites within
      update_psi_charge_pw_cpu/gpu (which remain in this file for now)
      continue to work because the function declaration is still in
      spin_constrain.h.
    
    The PW includes (hsolver_pw.h, elecstate_pw.h, onsite_proj.h,
    math_kernel_op.h) are still needed by cal_mw_from_lambda.cpp for
    update_psi_charge_pw_cpu/gpu; they will be removed in step 4 once
    those functions are also relocated.
    
    Verified: make -j 30 in build_max_para_test compiles cleanly.

--- source/source_pw/module_pwdft/deltaspin_pw_impl.cpp
+++ source/source_pw/module_pwdft/deltaspin_pw_impl.cpp
@@ -2,6 +2,7 @@
 #include "source_base/parallel_reduce.h"
 #include "source_base/tool_title.h"
 #include "source_base/timer.h"
+#include "source_base/kernels/math_kernel_op.h"
 #include "source_pw/module_pwdft/onsite_proj.h"
 #include "source_lcao/module_deltaspin/spin_constrain.h"
 #include "source_io/module_parameter/parameter.h"
@@ -77,6 +78,190 @@ void SpinConstrain<std::complex<double>>::cal_mi_pw()
     Parallel_Reduce::reduce_double_allpool(PARAM.inp.kpar, GlobalV::NPROC_IN_POOL, &(this->Mi_[0][0]), 3 * this->Mi_.size());
 
     ModuleBase::timer::end("spinconstrain::SpinConstrain", "cal_mi_pw");
+}
+
+/**
+ * @brief Compute DeltaSpin correction to the subspace Hamiltonian.
+ *
+ * @details Adds the constraint term to H in the projector subspace:
+ *   H += becp^† * ps, where ps = delta_lambda * becp
+ *
+ * For non-collinear (npol=2), this implements the full 2x2 Pauli matrix:
+ *   H_delta = | lambda_z     lambda_x + i*lambda_y |
+ *             | lambda_x - i*lambda_y   -lambda_z  |
+ *
+ * For collinear (npol=1), only the diagonal z-component with spin_sign:
+ *   H_delta = lambda_z * spin_sign
+ *
+ * @param h_tmp Subspace Hamiltonian (nbands x nbands, modified in place)
+ * @param becp_k Projector coefficients for k-point ik
+ * @param delta_lambda Lambda change per atom (or full lambda if full_update)
+ * @param nbands Number of bands
+ * @param nkb Total number of projectors
+ * @param nh_iat Number of projectors per atom
+ * @param ik K-point index (for spin_sign lookup in collinear mode)
+ * @param full_update If true, compute delta = lambda_current - lambda_at_save
+ */
+template <>
+void SpinConstrain<std::complex<double>>::calculate_delta_hcc(std::complex<double>* h_tmp, const std::complex<double>* becp_k, const ModuleBase::Vector3<double>* delta_lambda, const int nbands, const int nkb, const int* nh_iat, const int ik, bool full_update)
+{
+    ModuleBase::TITLE("spinconstrain::SpinConstrain", "calculate_delta_hcc");
+    ModuleBase::timer::start("spinconstrain::SpinConstrain", "calculate_delta_hcc");
+
+    // If full_update, compute actual delta = lambda_current - lambda_at_save
+    // This applies only the CHANGE in lambda, not the full lambda value
+    std::vector<ModuleBase::Vector3<double>> actual_delta;
+    const ModuleBase::Vector3<double>* effective_lambda = delta_lambda;
+    if (full_update)
+    {
+        int nat = this->get_nat();
+        actual_delta.resize(nat);
+        for (int iat = 0; iat < nat; iat++)
+        {
+            actual_delta[iat] = delta_lambda[iat] - this->lambda_in_sub_[iat];
+        }
+        effective_lambda = actual_delta.data();
+    }
+
+    int sum = 0; // Running sum of projectors across atoms
+    int size_ps = nkb * this->npol_ * nbands; // Total size of ps array
+    std::complex<double>* becp_cpu = nullptr;
+
+    // Handle GPU/CPU memory for becp
+    if(PARAM.inp.device == "gpu")
+    {
+#if ((defined __CUDA) || (defined __ROCM))
+        base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(becp_cpu, size_ps);
+        base_device::memory::synchronize_memory_op<std::complex<double>, base_device::DEVICE_CPU, base_device::DEVICE_GPU>()(becp_cpu, becp_k, size_ps);
+#endif
+    }
+    else if (PARAM.inp.device == "cpu")
+    {
+        becp_cpu = const_cast<std::complex<double>*>(becp_k);
+    }
+
+    // Compute modified projector coefficients: ps = delta_lambda * becp
+    std::vector<std::complex<double>> ps(size_ps, 0.0);
+    if(this->npol_ == 2)
+    {
+        // =============================================================
+        // nspin=4 (non-collinear): full Pauli matrix treatment
+        // =============================================================
+        // For each atom, construct 2x2 coefficients:
+        //   | lambda_z      lambda_x + i*lambda_y |
+        //   | lambda_x - i*lambda_y   -lambda_z   |
+        // Then: ps_up = coeff0 * becp_up + coeff2 * becp_dn
+        //        ps_dn = coeff1 * becp_up + coeff3 * becp_dn
+        for (int iat = 0; iat < this->Mi_.size(); iat++)
+        {
+            const int nproj = nh_iat[iat];
+            const std::complex<double> coefficients0(effective_lambda[iat][2], 0.0);
+            const std::complex<double> coefficients1(effective_lambda[iat][0] , effective_lambda[iat][1]);
+            const std::complex<double> coefficients2(effective_lambda[iat][0] , -1 * effective_lambda[iat][1]);
+            const std::complex<double> coefficients3(-1 * effective_lambda[iat][2], 0.0);
+            for (int ib = 0; ib < nbands * this->npol_; ib += this->npol_)
+            {
+                for (int ip = 0; ip < nproj; ip++)
+                {
+                    const int becpind = ib * nkb + sum + ip;
+                    const std::complex<double> becp1 = becp_cpu[becpind];
+                    const std::complex<double> becp2 = becp_cpu[becpind + nkb];
+                    ps[becpind] += coefficients0 * becp1
+                                    + coefficients2 * becp2;
+                    ps[becpind + nkb] += coefficients1 * becp1
+                                        + coefficients3 * becp2;
+                }
+            }
+            sum += nproj;
+        }
+    }
+    else if(this->npol_ == 1)
+    {
+        // =============================================================
+        // nspin=2 (collinear): only z-component with spin_sign
+        // =============================================================
+        // ps = lambda_z * spin_sign * becp
+        // spin_sign = +1 for spin-up k-points, -1 for spin-down
+        for (int iat = 0; iat < this->Mi_.size(); iat++)
+        {
+            const int nproj = nh_iat[iat];
+            double coefficients0 = effective_lambda[iat][2] * this->get_spin_sign(ik);
+            for (int ib = 0; ib < nbands; ib++)
+            {
+                for (int ip = 0; ip < nproj; ip++)
+                {
+                    const int becpind = ib * nkb + sum + ip;
+                    const std::complex<double> becp1 = becp_cpu[becpind];
+                    ps[becpind] += coefficients0 * becp1;
+                }
+            }
+            sum += nproj;
+        }
+    }
+
+    // Copy ps to GPU if needed
+    std::complex<double>* ps_pointer = nullptr;
+    if(PARAM.inp.device == "gpu")
+    {
+#if ((defined __CUDA) || (defined __ROCM))
+        base_device::memory::resize_memory_op<std::complex<double>, base_device::DEVICE_GPU>()(ps_pointer, size_ps);
+        base_device::memory::synchronize_memory_op<std::complex<double>, base_device::DEVICE_GPU, base_device::DEVICE_CPU>()(ps_pointer, ps.data(), size_ps);
+#endif
+    }
+    else if (PARAM.inp.device == "cpu")
+    {
+        ps_pointer = ps.data();
+    }
+
+    // =============================================================
+    // H += becp^† * ps (GEMM: C = alpha * A^† * B + beta * C)
+    // A = becp_k (npm x nbands), B = ps (npm x nbands), C = h_tmp (nbands x nbands)
+    // =============================================================
+    char transa = 'C'; // Conjugate transpose of becp
+    char transb = 'N'; // Normal ps
+    const int npm = nkb * this->npol_;
+    if (PARAM.inp.device == "gpu")
+    {
+#if ((defined __CUDA) || (defined __ROCM))
+        ModuleBase::gemm_op<std::complex<double>, base_device::DEVICE_GPU>()(
+            transa,
+            transb,
+            nbands,
+            nbands,
+            npm,
+            &ModuleBase::ONE,
+            becp_k,
+            npm,
+            ps_pointer,
+            npm,
+            &ModuleBase::ONE,
+            h_tmp,
+            nbands
+        );
+        base_device::memory::delete_memory_op<std::complex<double>, base_device::DEVICE_GPU>()(ps_pointer);
+        base_device::memory::delete_memory_op<std::complex<double>, base_device::DEVICE_CPU>()(becp_cpu);
+#endif
+
+    }
+    else if (PARAM.inp.device == "cpu")
+    {
+        ModuleBase::gemm_op<std::complex<double>, base_device::DEVICE_CPU>()(
+            transa,
+            transb,
+            nbands,
+            nbands,
+            npm,
+            &ModuleBase::ONE,
+            becp_k,
+            npm,
+            ps_pointer,
+            npm,
+            &ModuleBase::ONE,
+            h_tmp,
+            nbands
+        );
+    }
+    ModuleBase::timer::end("spinconstrain::SpinConstrain", "calculate_delta_hcc");
 }
 
 } // namespace spinconstrain
