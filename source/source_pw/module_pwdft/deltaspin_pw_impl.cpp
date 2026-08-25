12172788407ad9a279fc2753a19dbf8a411143c1
abacus_fixer <mohanchen@pku.eud.cn>
2026-08-25 11:42:06 +0800

    deltaspin: add empty deltaspin_pw_impl.cpp in module_pwdft
    Create a placeholder file source/source_pw/module_pwdft/deltaspin_pw_impl.cpp
    that will host PW-specific SpinConstrain member function implementations
    in subsequent commits:
      - cal_mi_pw()
      - calculate_delta_hcc()
      - update_psi_charge_pw_cpu()
      - update_psi_charge_pw_gpu()
    
    Update source/source_pw/module_pwdft/CMakeLists.txt to include the new
    source file in the module_pwdft build list.
    
    The file currently contains only the spinconstrain namespace skeleton
    with a comment listing the functions to be moved. No functional change;
    the build produces an empty object file.
    
    Verified: make -j 30 in build_max_para_test compiles cleanly and
    generates deltaspin_pw_impl.cpp.o.

---
+++ source/source_pw/module_pwdft/deltaspin_pw_impl.cpp
@@ -0,0 +1,10 @@
+#include "source_lcao/module_deltaspin/spin_constrain.h"
+
+namespace spinconstrain {
+// PW-specific SpinConstrain member function implementations will be
+// moved here in subsequent commits:
+//   - cal_mi_pw()
+//   - calculate_delta_hcc()
+//   - update_psi_charge_pw_cpu()
+//   - update_psi_charge_pw_gpu()
+} // namespace spinconstrain
