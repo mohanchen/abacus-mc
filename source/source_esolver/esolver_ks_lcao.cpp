e22c1b1ed63357f44d8e56eaeb9017b9f42c3221
abacus_fixer <mohanchen@pku.eud.cn>
2026-09-08 16:45:46 +0800

    fix(esolver): add .get() for unique_ptr dftu_ in init_dftu_lcao call
    Merge from develop reverted the raw pointer back to unique_ptr member
    in init_dftu_lcao call site, causing compile error on void* parameter.

--- source/source_esolver/esolver_ks_lcao.cpp
+++ source/source_esolver/esolver_ks_lcao.cpp
@@ -389,7 +389,7 @@ void ESolver_KS_LCAO<TK, TR>::iter_init(UnitCell& ucell, const int istep, const 
     }
 #endif
 
-    init_dftu_lcao<TK>(istep, iter, this->inp_->dft_plus_u, this->dftu_, this->dmat.dm, ucell, this->chr.rho, this->pw_rho->nrxx);
+    init_dftu_lcao<TK>(istep, iter, this->inp_->dft_plus_u, this->dftu_.get(), this->dmat.dm, ucell, this->chr.rho, this->pw_rho->nrxx);
 
 #ifdef __MLALGO
     // the density matrixes of DeePKS have been updated in each iter
