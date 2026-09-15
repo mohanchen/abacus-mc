7bcb2e05db13b3a4ce7342a663c5b73930b2ad2f
abacus_fixer <mohanchen@pku.eud.cn>
2026-09-15 17:26:13 +0800

    fix(force): update symmetrize_force_cartesian call to new signature
    The call site in force_pw.cpp still passed (ucell, p_symm, force); update
    to (p_symm, this->nat, force) to match the new signature that takes lattice
    vectors from the Symmetry object.

--- source/source_pw/module_pwdft/force_pw.cpp
+++ source/source_pw/module_pwdft/force_pw.cpp
@@ -195,7 +195,7 @@ void Forces<FPTYPE, Device>::cal_force(UnitCell& ucell,
 
     if (ModuleSymmetry::Symmetry::symm_flag == 1)
     {
-        ModuleSymmetry::symmetrize_force_cartesian(ucell, p_symm, force);
+        ModuleSymmetry::symmetrize_force_cartesian(p_symm, this->nat, force);
     }
 
     GlobalV::ofs_running << std::setiosflags(std::ios::fixed) << std::setprecision(6) << std::endl;
