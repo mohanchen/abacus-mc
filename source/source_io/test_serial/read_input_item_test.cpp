f798da51752aaae0ff22a7be2caf1da68f872f1f
abacus_fixer <mohanchen@pku.eud.cn>
2026-09-26 07:45:34 +0800

    Update tests for unified out_dos and removed TDOS/PDOS.dat
    - Add out_dos=2 deprecation test in read_input_item_test.cpp
    - Change scf_out_dos_spin2/INPUT from out_dos=2 to out_dos=1
    - Remove obsolete TDOS.dat and PDOS.dat from the reference output

--- source/source_io/test_serial/read_input_item_test.cpp
+++ source/source_io/test_serial/read_input_item_test.cpp
@@ -566,6 +566,12 @@ TEST_F(InputTest, Item_test)
         it->second.reset_value(it->second, param);
         EXPECT_EQ(param.input.out_dos, 0);
 
+        param.input.out_dos = 2;
+        testing::internal::CaptureStdout();
+        EXPECT_EXIT(it->second.check_value(it->second, param), ::testing::ExitedWithCode(1), "");
+        output = testing::internal::GetCapturedStdout();
+        EXPECT_THAT(output, testing::HasSubstr("NOTICE"));
+
         param.input.out_dos = 3;
         testing::internal::CaptureStdout();
         EXPECT_EXIT(it->second.check_value(it->second, param), ::testing::ExitedWithCode(1), "");
