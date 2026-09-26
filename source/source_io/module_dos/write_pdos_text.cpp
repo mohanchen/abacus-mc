3bd4ed77fc5045afc023eb188bc71c07df49dd7a
abacus_fixer <mohanchen@pku.eud.cn>
2026-09-26 10:11:40 +0800

    Add istep and npoints to PDOS file header

--- source/source_io/module_dos/write_pdos_text.cpp
+++ source/source_io/module_dos/write_pdos_text.cpp
@@ -41,6 +41,8 @@ void ModuleIO::write_pdos_text(
         // iw2m stores m as 0..2l, mapping to physical m values:
         // 0->0, 1->+1, 2->-1, 3->+2, 4->-2, 5->+3, 6->-3
         // So columns are ordered: s, p(m=0,+1,-1), d(m=0,+1,-1,+2,-2), f(...)
+        ofs << "# istep: " << istep + 1 << std::endl;
+        ofs << "# npoints: " << npoints << std::endl;
         ofs << "# energy(eV)  atom  species  pdos(1/eV), columns: s(m=0) p(m=0,+1,-1) d(m=0,+1,-1,+2,-2) f(m=0,+1,-1,+2,-2,+3,-3)" << std::endl;
 
         for (int iat = 0; iat < ucell.nat; ++iat)
